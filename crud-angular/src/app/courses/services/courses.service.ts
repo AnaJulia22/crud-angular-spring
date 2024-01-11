afbdc5f9483725fec0675831bedfe60b9340c584
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-11 20:42:21 -0300

    conectando front com bd do back

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/course';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -16,7 +16,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      //delay(5000),
+      //delay(5000)
       tap(courses => console.log(courses))
     );
   }
