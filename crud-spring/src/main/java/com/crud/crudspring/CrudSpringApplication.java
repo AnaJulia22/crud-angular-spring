3bb5500834aaaa6726166b93057a01c6e5599232
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-11 18:36:15 -0300

    inicio do bando de dados

--- crud-spring/src/main/java/com/crud/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/crud/crudspring/CrudSpringApplication.java
@@ -1,13 +1,32 @@
 package com.crud.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.crud.crudspring.model.Course;
+import com.crud.crudspring.reository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
---
+++ crud-spring/src/main/java/com/crud/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.crud.crudspring.controller;
+
+
+import java.util.List;
+
+import com.crud.crudspring.model.*;
+import com.crud.crudspring.reository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+@AllArgsConstructor
+public class CourseController {
+
+    private CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
