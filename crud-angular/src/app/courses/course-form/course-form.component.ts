e809a60cf60a7cfff30e9c82532eaca034721401
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-12 17:28:18 -0300

    adicao de pagina de form

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-course-form',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  onSubmit() {
+    
+  }
+
+  onClean() {}
+
+  onCancel() {}
 
 }
