9c633369fd84e3d18d1fc9ae96e184dd5e0689d7
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-13 01:43:48 -0300

    tipando form

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NonNullableFormBuilder } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CoursesService } from '../services/courses.service';
@@ -13,20 +13,17 @@ import { Location } from '@angular/common';
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
+  form= this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private _snackBar: MatSnackBar,
     private location: Location
-    ) {
-
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
-  }
+    ) {}
 
   onSubmit() {
     this.service.save(this.form.value).subscribe({
