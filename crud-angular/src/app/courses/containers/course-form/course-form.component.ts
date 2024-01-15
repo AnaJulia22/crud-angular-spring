116058d6f672d8e71a1e6f308245b8b067e51a9c
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-15 17:53:32 -0300

    adiciona verificacao de remocao e validacao de campos do form

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -14,7 +14,14 @@ import { CoursesService } from '../../services/courses.service';
 })
 export class CourseFormComponent {
 
-  form!: FormGroup;
+  form = this.formBuilder.group({
+      _id: [''],
+      name: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)]],
+      category: ['', [Validators.required]]
+    });
 
   constructor(
     private formBuilder: NonNullableFormBuilder,
@@ -25,16 +32,12 @@ export class CourseFormComponent {
     //public formUtils: FormUtilsService
     ) {
       const course: Course = this.route.snapshot.data['course'];
-      this.form = this.formBuilder.group({
-      _id: [course._id],
-      name: [
-        course.name,
-        [Validators.required, Validators.minLength(5), Validators.maxLength(100)]
-      ],
-      category: [course.category, [Validators.required]],
-      //lessons: this.formBuilder.array(this.retrieveLessons(course), Validators.required)
-    });
-    }
+      this.form.setValue({
+        _id: course._id,
+        name: course.name,
+        category: course.category
+      })
+    };
 
   onSubmit() {
     //if (this.form.valid) {
@@ -65,6 +68,25 @@ export class CourseFormComponent {
 
   private onError() {
     this._snackBar.open("Erro ao salvar curso","",{duration:5000});
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+    if (field?.hasError('required')) {
+      return "Campo obrigatório";
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
+      return `Tamanho maxímo precisa ser de ${requiredLength} caracteres`;
+    }
+
+    return "Campo inválido";
   }
 
 }
--- crud-angular/src/app/courses/containers/courses/courses.component.ts
+++ crud-angular/src/app/courses/containers/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -50,17 +51,25 @@ export class CoursesComponent {
     }
 
     onRemove(course: Course) {
-      this.coursesService.remove(course._id).subscribe({
-        next: data => {
-          this.refresh();
-          this._snackBar.open("Curso removido com sucesso","X",{
-            duration:5000,
-            verticalPosition: 'top',
-            horizontalPosition: 'center'
-          });
-        },
-        error: error => this.onError('Erro ao tentar remover curso')
-      })
-    }
 
+      const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+        data: 'Tem certeza que gostaria de remover o curso?'
+      });
+
+      dialogRef.afterClosed().subscribe((result: boolean) => {
+        if (result) {
+          this.coursesService.remove(course._id).subscribe({
+            next: data => {
+              this.refresh();
+              this._snackBar.open("Curso removido com sucesso","X",{
+                duration:5000,
+                verticalPosition: 'top',
+                horizontalPosition: 'center'
+              });
+            },
+            error: error => this.onError('Erro ao tentar remover curso')
+          });
+        }
+      });
+    }
 }
