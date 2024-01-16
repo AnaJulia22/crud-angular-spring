2b948efcf7f0410132e71369c4aae9c8cb46708d
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-16 16:32:41 -0300

    adiciona um dto

--- crud-spring/src/main/java/com/crud/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/crud/crudspring/controller/CourseController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.crud.crudspring.model.Course;
+import com.crud.crudspring.dto.CourseDTO;
 import com.crud.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
@@ -35,23 +35,23 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<Course> list() {
+    public List<CourseDTO> list() {
         return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public Course findById(@PathVariable @NotNull @Positive Long id) {
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
         return courseService.findById(id);
     }
     
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course) {
+    public CourseDTO create(@RequestBody @Valid @NotNull CourseDTO course) {
         return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public Course update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
+    public CourseDTO update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid @NotNull CourseDTO course) {
         return courseService.update(id, course);
     }
 
